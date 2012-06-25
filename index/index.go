9c83809b77ead364664680a4fcb574932c8d19d4
jingmi <nosqldev@gmail.com>
2012-06-25 15:54:25 +0800

    index module done

--- index/index.go
+++ index/index.go
@@ -12,55 +12,111 @@
 package index
 
 import (
-    /*"io"*/
-    /*"encoding/binary"*/
+    "io"
+    "encoding/binary"
+    "sort"
+)
+
+const (
+    default_size = 1000000
+    max_near_poi_count = 10000
 )
 
 type poi_1d_item struct {
     XY float64 /* X or Y of a given POI */
-    ID uint32
+    ID uint32  /* index referred to GuidArray belongs to POI_index */
 }
+type poi_1d_slice []poi_1d_item
 type POI_Item struct {
-    GUID string
+    GUID uint64
     X float64
     Y float64
 }
 type POI_index struct {
     PoiXIdx []poi_1d_item
     PoiYIdx []poi_1d_item
-    GuiArray []POI_Item
+    GuidArray []POI_Item
 }
 
 /* {{{ verifyIndex(poi_idx POI_index) bool  */
 
+// this func is just designed for test case
 func verifyIndex(poi_idx POI_index) bool {
     return ((len(poi_idx.PoiXIdx) == len(poi_idx.PoiYIdx)) &&
-            (len(poi_idx.PoiXIdx) == len(poi_idx.GuiArray)));
+            (len(poi_idx.PoiXIdx) == len(poi_idx.GuidArray)));
 }
 
 /* }}} */
 /* {{{ LoadPOI(reader io.Reader) (poi_idx *POI_index, retval int) */
 
-/*
- *func LoadPOI(reader io.Reader) (poi_idx *POI_index, retval int) {
- *    var tmp_X, tmp_Y float64
- *    retval = 0
- *    poi_idx = new(POI_index)
- *    poi_idx[0] = make(poi_1d_index, 0, 100000)
- *    poi_idx[1] = make(poi_1d_index, 0, 100000)
- *
- *    for i:=0; ; i++ {
- *        err := binary.Read(reader, binary.LittleEndian, &tmp_X)
- *        if err != nil { break }
- *        err = binary.Read(reader, binary.LittleEndian, &tmp_Y)
- *        if err != nil { retval = -1; break }
- *        poi_idx[0] = append(poi_idx[0], tmp_X)
- *        poi_idx[1] = append(poi_idx[1], tmp_Y)
- *    }
- *
- *    return poi_idx, retval
- *}
- */
+func LoadPOI(reader io.Reader) (poi_idx *POI_index, retval int) {
+    var tmp_X, tmp_Y float64
+    var guid uint64
+    var poi_item POI_Item
+    var poi_x_item poi_1d_item
+    var poi_y_item poi_1d_item
+    retval = 0
+    poi_idx = new(POI_index)
+    poi_idx.PoiXIdx = make([]poi_1d_item, 0, default_size)
+    poi_idx.PoiYIdx = make([]poi_1d_item, 0, default_size)
+    poi_idx.GuidArray = make([]POI_Item, 0, default_size)
+
+    for i:=uint32(0); ; i++ {
+        err := binary.Read(reader, binary.LittleEndian, &guid)
+        if err != nil { break }
+        err = binary.Read(reader, binary.LittleEndian, &tmp_X)
+        if err != nil { retval = -1; break }
+        err = binary.Read(reader, binary.LittleEndian, &tmp_Y)
+        if err != nil { retval = -2; break }
+
+        poi_item.GUID = guid
+        poi_item.X = tmp_X
+        poi_item.Y = tmp_Y
+        poi_x_item.XY = tmp_X
+        poi_x_item.ID = i
+        poi_y_item.XY = tmp_Y
+        poi_y_item.ID = i
+
+        poi_idx.GuidArray = append(poi_idx.GuidArray, poi_item)
+        poi_idx.PoiXIdx   = append(poi_idx.PoiXIdx, poi_x_item)
+        poi_idx.PoiYIdx   = append(poi_idx.PoiYIdx, poi_y_item)
+    }
+
+    sortPoiIndex(poi_idx)
+
+    return poi_idx, retval
+}
+
+/* }}} */
+/* {{{ sortPoiIndex(poi_idx *POI_index)  */
+
+func sortPoiIndex(poi_idx *POI_index) {
+    sort.Sort(poi_1d_slice(poi_idx.PoiXIdx))
+    sort.Sort(poi_1d_slice(poi_idx.PoiYIdx))
+}
+
+/* }}} */
+
+/* {{{ (slice poi_1d_slice) Len () int */
+
+func (slice poi_1d_slice) Len () int{
+    return len(slice)
+}
+
+/* }}} */
+/* {{{ (slice poi_1d_slice) Less(i,j int) bool */
+
+func (slice poi_1d_slice) Less(i,j int) bool{
+    if slice[i].XY < slice[j].XY { return true }
+    return false
+}
+
+/* }}} */
+/* {{{ (slice poi_1d_slice) Swap(i,j int)  */
+
+func (slice poi_1d_slice) Swap(i,j int) {
+    slice[i], slice[j] = slice[j], slice[i]
+}
 
 /* }}} */
 
--- index/index_test.go
+++ index/index_test.go
@@ -13,8 +13,8 @@ package index
 
 import(
     "testing"
-    /*"bytes"*/
-    /*"encoding/binary"*/
+    "bytes"
+    "encoding/binary"
 )
 
 /* {{{ Test_verifyIndex(t *testing.T) */
@@ -30,8 +30,8 @@ func Test_verifyIndex(t *testing.T) {
             {3.14159265, 1},
         },
         []POI_Item {
-            {"123", 100.0, 3.0},
-            {"789", 200.0, 3.14159265},
+            {123, 100.0, 3.0},
+            {789, 200.0, 3.14159265},
         },
     }
     if !verifyIndex(poi_idx1) {
@@ -48,9 +48,9 @@ func Test_verifyIndex(t *testing.T) {
             {3.14159265, 1},
         },
         []POI_Item {
-            {"123", 100.0, 3.0},
-            {"789", 200.0, 3.14159265},
-            {"abc", 1, 1},
+            {123, 100.0, 3.0},
+            {789, 200.0, 3.14159265},
+            {111, 1, 1},
         },
     }
     if verifyIndex(poi_idx2) {
@@ -61,24 +61,39 @@ func Test_verifyIndex(t *testing.T) {
 /* }}} */
 /* {{{ TestLoadPOI(t *testing.T)  */
 
-/*
- *func TestLoadPOI(t *testing.T) {
- *    data := make([]byte, 0, 1000)
- *    buf  := bytes.NewBuffer(data)
- *
- *    binary.Write(buf, binary.LittleEndian, float64(1.0))
- *    binary.Write(buf, binary.LittleEndian, float64(200.0))
- *    binary.Write(buf, binary.LittleEndian, float64(19.0))
- *    binary.Write(buf, binary.LittleEndian, float64(3.14159265))
- *
- *    poi_idx, err := LoadPOI(buf)
- *    if poi_idx[0][0] != 1.0 { t.Fatal("LoadPOI() failed") }
- *    if poi_idx[1][0] != 200.0 { t.Fatal("LoadPOI() failed") }
- *    if poi_idx[0][1] != 19.0 { t.Fatal("LoadPOI() failed") }
- *    if poi_idx[1][1] != 3.14159265 { t.Fatal("LoadPOI() failed") }
- *    if err != 0 { t.Fatal("LoadPOI() failed") }
- *}
- */
+func TestLoadPOI(t *testing.T) {
+    data := make([]byte, 0, 1000)
+    buf  := bytes.NewBuffer(data)
+
+    binary.Write(buf, binary.LittleEndian, uint64(1))
+    binary.Write(buf, binary.LittleEndian, float64(1.0))
+    binary.Write(buf, binary.LittleEndian, float64(200.0))
+
+    binary.Write(buf, binary.LittleEndian, uint64(2))
+    binary.Write(buf, binary.LittleEndian, float64(19.0))
+    binary.Write(buf, binary.LittleEndian, float64(3.14159265))
+
+    poi_idx, err := LoadPOI(buf)
+
+    if err != 0 { t.Fatal("LoadPOI() failed") }
+
+    if poi_idx.PoiXIdx[0].XY != 1.0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.PoiXIdx[0].ID != 0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.PoiXIdx[1].XY != 19.0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.PoiXIdx[1].ID != 1 { t.Fatal("LoadPOI() failed") }
+
+    if poi_idx.PoiYIdx[0].XY != 3.14159265 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.PoiYIdx[0].ID != 1 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.PoiYIdx[1].XY != 200.0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.PoiYIdx[1].ID != 0 { t.Fatal("LoadPOI() failed") }
+
+    if poi_idx.GuidArray[0].GUID != 1 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.GuidArray[0].X != 1.0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.GuidArray[0].Y != 200.0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.GuidArray[1].GUID != 2 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.GuidArray[1].X != 19.0 { t.Fatal("LoadPOI() failed") }
+    if poi_idx.GuidArray[1].Y != 3.14159265 { t.Fatal("LoadPOI() failed") }
+}
 
 /* }}} */
 
