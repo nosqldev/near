6cd99ecaa4be44d83bc8b46003161878ec3549d3
jingmi <nosqldev@gmail.com>
2012-06-28 03:24:29 +0800

    Functions to be called in FetchNearPOI() is done

--- top/topnear.go
+++ top/topnear.go
@@ -13,39 +13,147 @@ package top
 
 import(
     "sort"
+    "math"
     . "../index"
 )
 
 const max_near_poi_count = 10000
 
+type distanceTable_t map[POI_Item]float64
+type sort_poi_t struct {
+    ID uint32
+    Key POI_Item
+    Distance float64
+}
+type sort_poi_slice_t []sort_poi_t
+
 /* {{{ FetchNearPOI(poi_idx *POI_index, X float64, Y float64, count int) (guid_slice []uint64, retval int)  */
 
-func FetchNearPOI(poi_idx *POI_index, X float64, Y float64, count int) (guid_slice []uint64, retval int) {
+func FetchNearPOI(poi_idx *POI_index, x float64, y float64, count int) (guid_slice []uint64, retval int) {
     retval = 0
 
-    /*
-     *if count > max_near_poi_count/2 { return nil, -100 }
-     *result_x, retcode := fetch_from_index(poi_idx.PoiXIdx, X, count)
-     *if retcode != 0 { return nil, -1 }
-     *result_y, retcode := fetch_from_index(poi_idx.PoiYIdx, Y, count)
-     *if retcode != 0 { return nil, -2 }
-     */
-    /*
-     *result := intersect(result_x, result_y)
-     *farthest_poi, cache_distances := find_farthest_poi(poi_idx.GuidArray, result)
-     *result = scan_near_poi(poi_idx, farthest_poi, cache_distances)
-     *sortby_distance(poi_idx.GuidArray, result)
-     *guid_slice = translate_guid(poi_idx.GuidArray, result)
-     */
-
+/*
+ *    if count > max_near_poi_count/2 { return nil, -100 }
+ *    result_x, retcode := fetch_from_index_by_count(poi_idx.PoiXIdx, x, count)
+ *    if retcode != 0 { return nil, -1 }
+ *    result_y, retcode := fetch_from_index_by_count(poi_idx.PoiYIdx, y, count)
+ *    if retcode != 0 { return nil, -2 }
+ *    result := intersect(result_x, result_y)
+ *    farthest_point, max_distance, cache_distances := find_farthest_poi(poi_idx.GuidArray, x, y, result)
+ *    result = ScanNearPOI(x, y, poi_idx, count, max_distance, cache_distances)
+ *    guid_slice = translate_guid(poi_idx.GuidArray, result, x, y)
+ *
+ */
     return
 }
 
 /* }}} */
+/* {{{ ScanNearPOI(x float64, y float64, poi_idx *POI_index, count int, max_distance float64, cache_distances distanceTable_t) []uint32  */
 
-/* {{{ fetch_from_index(poi_1d_slice Poi_1d_slice_t, v float64, count int) ([]uint32, int)  */
+func ScanNearPOI(x float64, y float64, poi_idx *POI_index, count int, max_distance float64, cache_distances distanceTable_t) []uint32 {
+    result_x := fetch_from_index_by_range(poi_idx.PoiXIdx, x, max_distance)
+    if result_x == nil { return nil }
+    result_y := fetch_from_index_by_range(poi_idx.PoiYIdx, y, max_distance)
+    if result_y == nil { return nil }
 
-func fetch_from_index(poi_1d_slice Poi_1d_slice_t, v float64, count int) ([]uint32, int) {
+    result := intersect(result_x, result_y)
+    if result == nil { return nil }
+    result = filter_by_distance(x, y, poi_idx.GuidArray, result, max_distance, cache_distances)
+
+    var result_size int
+    if count == 0 {
+        result_size = len(result)
+    } else {
+        result_size = int(math.Min(float64(count), float64(len(result))))
+    }
+    sortby_distance(poi_idx.GuidArray, result[:result_size], cache_distances)
+
+    return result[:result_size]
+}
+
+/* }}} */
+
+/* {{{ sortby_distance(guid_slice []POI_Item, ids []uint32, cache_distances distanceTable_t)  */
+
+/* This function will drop those id which are not caculated in cache_distances!
+ * Basically, the distances corresponding to the points to be sorted should be caculated before.
+ */
+func sortby_distance(guid_slice []POI_Item, ids []uint32, cache_distances distanceTable_t) {
+    sort_slice := make(sort_poi_slice_t, 0, len(ids))
+    var sort_item sort_poi_t
+    for _, id := range ids {
+        distance, exists := cache_distances[ guid_slice[id] ]
+        if !exists { continue /* TODO Throw exception here */ }
+        sort_item.ID = id
+        sort_item.Key = guid_slice[id]
+        sort_item.Distance = distance
+        sort_slice = append(sort_slice, sort_item)
+    }
+    sort.Sort(sort_slice)
+
+    for id, item := range sort_slice {
+        ids[id] = item.ID
+    }
+    /* set the tailing ids slice to zero POI if querying non-existence item in cache_distances */
+    for i := len(sort_slice); i < len(ids); i++ {
+        ids[i] = 0
+    }
+}
+
+/* }}} */
+/* {{{ (s sort_poi_slice_t) Len() int  */
+
+func (s sort_poi_slice_t) Len() int {
+    return len(s)
+}
+
+/* }}} */
+/* {{{ (s sort_poi_slice_t) Less(i,j int) bool */
+
+func (s sort_poi_slice_t) Less(i,j int) bool{
+    return s[i].Distance < s[j].Distance
+}
+
+/* }}} */
+/* {{{ (s sort_poi_slice_t) Swap(i,j int)  */
+
+func (s sort_poi_slice_t) Swap(i,j int) {
+    s[i], s[j] = s[j], s[i]
+}
+
+/* }}} */
+/* {{{ fetch_from_index_by_range(poi_1d_slice Poi_1d_slice_t, v float64, max_distance float64) []uint32 */
+
+func fetch_from_index_by_range(poi_1d_slice Poi_1d_slice_t, v float64, max_distance float64) []uint32 {
+    pos := sort.Search(len(poi_1d_slice), func(i int)bool { return poi_1d_slice[i].XY >= v })
+    if pos >= len(poi_1d_slice) { pos = len(poi_1d_slice) - 1 }
+
+    var start, end int = -1, -1
+    for p := pos; p >= 0 && poi_1d_slice[p].XY >= v - max_distance; p-- {
+        start = p
+    }
+    for p := pos; p < len(poi_1d_slice) && poi_1d_slice[p].XY <= v + max_distance; p++ {
+        end = p
+    }
+    if start == -1 || end == -1 { return nil }
+    end ++
+    if end > start + max_near_poi_count { end = start + max_near_poi_count }
+
+    result := make([]uint32, 0, max_near_poi_count)
+    for i := start; i < end ; i++ {
+        result = append(result, poi_1d_slice[i].ID)
+    }
+
+    if len(result) == 0 {
+        return nil
+    }
+    return result
+}
+
+/* }}} */
+/* {{{ fetch_from_index_by_count(poi_1d_slice Poi_1d_slice_t, v float64, count int) ([]uint32, int)  */
+
+func fetch_from_index_by_count(poi_1d_slice Poi_1d_slice_t, v float64, count int) ([]uint32, int) {
     var retval int = 0
 
     pos := sort.Search(len(poi_1d_slice),
@@ -75,6 +183,98 @@ func fetch_from_index(poi_1d_slice Poi_1d_slice_t, v float64, count int) ([]uint
     }
 
     return result, retval
+}
+
+/* }}} */
+/* {{{ intersect(x_slice, y_slice []uint32) []uint32  */
+
+func intersect(x_slice, y_slice []uint32) []uint32 {
+    var result_len int = 0
+    if len(x_slice) > len(y_slice) {
+        result_len = len(x_slice)
+    } else {
+        result_len = len(y_slice)
+    }
+    var result []uint32 = make([]uint32, 0, result_len)
+    var x_set map[uint32]bool = make(map[uint32]bool)
+
+    for _, x := range x_slice {
+        x_set[x] = true
+    }
+    for _, y := range y_slice {
+        if _, err := x_set[y]; err {
+            result = append(result, y)
+        }
+    }
+
+    return result
+}
+
+/* }}} */
+/* {{{ find_farthest_poi(guid_slice []POI_Item, x float64, y float64, ids []uint32) (point POI_Item, max_distance float64, cache_distances distanceTable_t) */
+
+func find_farthest_poi(guid_slice []POI_Item, x float64, y float64, ids []uint32) (point POI_Item, max_distance float64, cache_distances distanceTable_t) {
+    max_distance = 0
+    cache_distances = make(distanceTable_t)
+
+    for _, id := range ids {
+        pnt := guid_slice[id]
+        cache_distances[pnt] = calc_distance(pnt.X, pnt.Y, x, y)
+        if max_distance < cache_distances[pnt] {
+            max_distance = cache_distances[pnt]
+            point = pnt
+        }
+    }
+
+    return
+}
+
+/* }}} */
+/* {{{ calc_distance(x1, y1, x2, y2 float64) distance float64  */
+
+func calc_distance(x1, y1, x2, y2 float64) float64 {
+    return math.Sqrt(math.Abs(x1 - x2) * math.Abs(x1 - x2) + math.Abs(y1 - y2) * math.Abs(y1 - y2))
+}
+
+/* }}} */
+/* {{{ filter_by_distance(x float64, y float64, guid_slice []POI_Item, ids []uint32, max_distance float64, cache_distances distanceTable_t) []uint32  */
+
+func filter_by_distance(x float64, y float64, guid_slice []POI_Item, ids []uint32, max_distance float64, cache_distances distanceTable_t) []uint32 {
+    var pnt POI_Item
+    var result_ids []uint32 = make([]uint32, 0, max_near_poi_count)
+
+    for _, id := range ids {
+        if id > uint32(len(guid_slice)) { continue }
+        pnt = guid_slice[id]
+        distance, exists := cache_distances[pnt]
+        if !exists {
+            distance = calc_distance(x, y, pnt.X, pnt.Y)
+            cache_distances[pnt] = distance
+        }
+
+        if distance <= max_distance {
+            result_ids = append(result_ids, id)
+        }
+    }
+
+    return result_ids
+}
+
+/* }}} */
+/* {{{ translate_guid(guid_slice []POI_Item, ids []uint32, x float64, y float64) []uint64  */
+
+func translate_guid(guid_slice []POI_Item, ids []uint32, x float64, y float64) []uint64 {
+    var result []uint64 = make([]uint64, 0, len(ids))
+
+    for _, id := range ids {
+        if guid_slice[id].X == x && guid_slice[id].Y == y {
+            /* filter the querying POI */
+            continue
+        }
+        result = append(result, guid_slice[id].GUID)
+    }
+
+    return result
 }
 
 /* }}} */
--- top/topnear_test.go
+++ top/topnear_test.go
@@ -16,6 +16,7 @@ import(
     "runtime"
     "testing"
     "strings"
+    "math"
     . "../index"
 )
 
@@ -40,10 +41,10 @@ func build_assert_func(mesg string, t *testing.T) (func(expr bool)int) {
 
 /* }}} */
 
-/* {{{ Test_fetch_from_index(t *testing.T)  */
+/* {{{ Test_fetch_from_index_by_count(t *testing.T)  */
 
-func Test_fetch_from_index(t *testing.T) {
-    assert := build_assert_func("fetch_from_index() failed", t)
+func Test_fetch_from_index_by_count(t *testing.T) {
+    assert := build_assert_func("fetch_from_index_by_count() failed", t)
     index_slice := Poi_1d_slice_t {
         {float64(1.0), 0},
         {float64(2.0), 1},
@@ -55,18 +56,18 @@ func Test_fetch_from_index(t *testing.T) {
         {float64(8.0), 7},
     }
 
-    result, retval := fetch_from_index(index_slice, 3.0, 2)
+    result, retval := fetch_from_index_by_count(index_slice, 3.0, 2)
     assert(retval == 0)
     assert(len(result) == 3)
     assert(result[0] == 1)
     assert(result[1] == 2)
     assert(result[2] == 3)
 
-    result, retval = fetch_from_index(index_slice, 3.0, 1)
+    result, retval = fetch_from_index_by_count(index_slice, 3.0, 1)
     assert(retval == 0)
     assert(len(result) == 3)
 
-    result, retval = fetch_from_index(index_slice, 3.0, 3)
+    result, retval = fetch_from_index_by_count(index_slice, 3.0, 3)
     assert(retval == 0)
     assert(len(result) == 5)
     assert(result[0] == 0)
@@ -75,21 +76,21 @@ func Test_fetch_from_index(t *testing.T) {
     assert(result[3] == 3)
     assert(result[4] == 4)
 
-    result, retval = fetch_from_index(index_slice, 7.0, 1)
+    result, retval = fetch_from_index_by_count(index_slice, 7.0, 1)
     assert(retval == 0)
     assert(len(result) == 3)
     assert(result[0] == 5)
     assert(result[1] == 6)
     assert(result[2] == 7)
 
-    result, retval = fetch_from_index(index_slice, 7.0, 2)
+    result, retval = fetch_from_index_by_count(index_slice, 7.0, 2)
     assert(retval == 0)
     assert(len(result) == 3)
     assert(result[0] == 5)
     assert(result[1] == 6)
     assert(result[2] == 7)
 
-    result, retval = fetch_from_index(index_slice, 7.0, 4)
+    result, retval = fetch_from_index_by_count(index_slice, 7.0, 4)
     assert(retval == 0)
     assert(len(result) == 4)
     assert(result[0] == 4)
@@ -97,7 +98,7 @@ func Test_fetch_from_index(t *testing.T) {
     assert(result[2] == 6)
     assert(result[3] == 7)
 
-    result, retval = fetch_from_index(index_slice, 7.0, 100)
+    result, retval = fetch_from_index_by_count(index_slice, 7.0, 100)
     assert(retval == 0)
     assert(len(result) == 8)
     assert(result[0] == 0)
@@ -108,6 +109,343 @@ func Test_fetch_from_index(t *testing.T) {
     assert(result[5] == 5)
     assert(result[6] == 6)
     assert(result[7] == 7)
+
+    result, retval = fetch_from_index_by_count(index_slice, -3.0, 2)
+    assert(retval == 0)
+    assert(result == nil)
+}
+
+/* }}} */
+/* {{{ Test_intersect(t *testing.T)  */
+
+func Test_intersect(t *testing.T) {
+    assert := build_assert_func("intersect() failed", t)
+
+    intersection := intersect([]uint32{1, 2, 3, 6, 0, 9}, []uint32{0, 8, 7, 3, 1})
+    assert(len(intersection) == 3)
+    assert(intersection[0] == 0)
+    assert(intersection[1] == 3)
+    assert(intersection[2] == 1)
+}
+
+/* }}} */
+/* {{{ Test_calc_distance(t *testing.T)  */
+
+func Test_calc_distance(t *testing.T) {
+    assert := build_assert_func("calc_distance() failed", t)
+
+    assert(5.0 == calc_distance(0.0, 3, 4.0, 0))
+    assert(13 == calc_distance(0, 5, 12, 0))
+}
+
+/* }}} */
+/* {{{ Test_find_farthest_poi(t *testing.T)  */
+
+func Test_find_farthest_poi(t *testing.T) {
+    assert := build_assert_func("find_farthest_poi() failed", t)
+
+    point, max_distance, cache_distances := find_farthest_poi(
+        []POI_Item {
+            {0, 4, 3}, {1, 5, 12}, {2, 9, 40}, {3, 7, 24},
+        },
+        0, 0, []uint32{0, 1, 2, 3})
+    assert(point == POI_Item{2, 9, 40})
+    assert(len(cache_distances) == 4)
+    assert(max_distance == 41)
+    assert(cache_distances[POI_Item{0, 4, 3}] == 5)
+    assert(cache_distances[POI_Item{1, 5, 12}] == 13)
+    assert(cache_distances[POI_Item{3, 7, 24}] == 25)
+    assert(cache_distances[POI_Item{2, 9, 40}] == 41)
+
+    point, max_distance, cache_distances = find_farthest_poi(
+        []POI_Item {
+            {0, 4, 3}, {1, 5, 12}, {2, 9, 40}, {3, 7, 24},
+        },
+        0, 0, []uint32{1, 0})
+    assert(point == POI_Item{1, 5, 12})
+    assert(max_distance == 13)
+    assert(len(cache_distances) == 2)
+    assert(cache_distances[POI_Item{0, 4, 3}] == 5)
+    assert(cache_distances[POI_Item{1, 5, 12}] == 13)
+}
+
+/* }}} */
+/* {{{ Test_fetch_from_index_by_range(t *testing.T)  */
+
+func Test_fetch_from_index_by_range(t *testing.T) {
+    assert := build_assert_func("fetch_from_index_by_range() failed", t)
+
+    index_slice := Poi_1d_slice_t{
+        {1.0, 0}, {2.0, 3}, {3.0, 7},
+        {4.0, 2}, {5.0, 1}, {6.0, 8},
+        {7.0, 4}, {8.0, 5}, {9.0, 6},}
+
+    result := fetch_from_index_by_range(index_slice, 3, 1)
+    assert(len(result) == 3)
+    assert(result[0] == 3)
+    assert(result[1] == 7)
+    assert(result[2] == 2)
+
+    result = fetch_from_index_by_range(index_slice, 2.5, 1)
+    assert(len(result) == 2)
+    assert(result[0] == 3)
+    assert(result[1] == 7)
+
+    result = fetch_from_index_by_range(index_slice, 3.0, 3)
+    assert(len(result) == 6)
+    assert(result[0] == 0)
+    assert(result[1] == 3)
+    assert(result[2] == 7)
+    assert(result[3] == 2)
+    assert(result[4] == 1)
+    assert(result[5] == 8)
+
+    result = fetch_from_index_by_range(index_slice, 6.0, 4)
+    assert(len(result) == 8)
+    assert(result[0] == 3)
+    assert(result[1] == 7)
+    assert(result[2] == 2)
+    assert(result[3] == 1)
+    assert(result[4] == 8)
+    assert(result[5] == 4)
+    assert(result[6] == 5)
+    assert(result[7] == 6)
+
+    result = fetch_from_index_by_range(index_slice, 9.0, 4)
+    assert(len(result) == 5)
+    assert(result[0] == 1)
+    assert(result[1] == 8)
+    assert(result[2] == 4)
+    assert(result[3] == 5)
+    assert(result[4] == 6)
+
+    result = fetch_from_index_by_range(index_slice, 31, 1)
+    assert(result == nil)
+
+    result = fetch_from_index_by_range(index_slice, 10, 3)
+    assert(len(result) == 3)
+    assert(result[0] == 4)
+    assert(result[1] == 5)
+    assert(result[2] == 6)
+
+    result = fetch_from_index_by_range(index_slice, 0, 1.5)
+    assert(len(result) == 1)
+    assert(result[0] == 0)
+}
+
+/* }}} */
+/* {{{ Test_filter_by_distance(t *testing.T)  */
+
+func Test_filter_by_distance(t *testing.T) {
+    assert := build_assert_func("filter_by_distance() failed", t)
+    guid_slice := []POI_Item {
+        {0, 3, 5},
+        {1, 4, 6},
+        {2, 6, 4},
+        {3, 2.5, 2.5},
+        {4, 4-math.Sqrt(2), 4-math.Sqrt(2)},
+        {5, 2.6, 2.6},
+        {6, 2, 2},
+        {7, 4, 2},
+        {8, 2, 4},
+    }
+    var cache distanceTable_t = make(distanceTable_t)
+
+    result := filter_by_distance(4, 4, guid_slice, []uint32{0, 1, 2, 3, 4, 5, 6, 7, 8}, 2, cache)
+    assert(len(result) == 7)
+    assert(len(cache) == 9)
+    assert(result[0] == 0)
+    assert(result[1] == 1)
+    assert(result[2] == 2)
+    assert(result[3] == 4)
+    assert(result[4] == 5)
+    assert(result[5] == 7)
+    assert(result[6] == 8)
+
+    for k, _ := range cache {
+        cache[k] = 1000
+    }
+    result = filter_by_distance(4, 4, guid_slice, []uint32{0, 1, 2, 3, 4, 5, 6, 7, 8}, 2, cache)
+    assert(len(result) == 0)
+    assert(len(cache) == 9)
+
+    cache = distanceTable_t{}
+    result = filter_by_distance(4, 4, guid_slice, []uint32{0, 1, 3, 4}, 2, cache)
+    assert(len(result) == 3)
+    assert(len(cache) == 4)
+    assert(result[0] == 0)
+    assert(result[1] == 1)
+    assert(result[2] == 4)
+}
+
+/* }}} */
+/* {{{ TestScanNearPOI(t testing.T)  */
+
+func TestScanNearPOI(t *testing.T) {
+    assert := build_assert_func("ScanNearPOI() failed", t)
+
+    poi_idx := new(POI_index)
+    poi_idx.PoiXIdx = make(Poi_1d_slice_t, 6)
+    poi_idx.PoiYIdx = make(Poi_1d_slice_t, 6)
+    poi_idx.GuidArray = make([]POI_Item, 6)
+
+    poi_idx.PoiXIdx[0].XY = 2
+    poi_idx.PoiXIdx[0].ID = 5
+    poi_idx.PoiXIdx[1].XY = 2.5
+    poi_idx.PoiXIdx[1].ID = 2
+    poi_idx.PoiXIdx[2].XY = 4-math.Sqrt(2)
+    poi_idx.PoiXIdx[2].ID = 3
+    poi_idx.PoiXIdx[3].XY = 2.6
+    poi_idx.PoiXIdx[3].ID = 4
+    poi_idx.PoiXIdx[4].XY = 3
+    poi_idx.PoiXIdx[4].ID = 0
+    poi_idx.PoiXIdx[5].XY = 4
+    poi_idx.PoiXIdx[5].ID = 1
+
+    poi_idx.PoiYIdx[0].XY = 2
+    poi_idx.PoiYIdx[0].ID = 5
+    poi_idx.PoiYIdx[1].XY = 2.5
+    poi_idx.PoiYIdx[1].ID = 2
+    poi_idx.PoiYIdx[2].XY = 4-math.Sqrt(2)
+    poi_idx.PoiYIdx[2].ID = 3
+    poi_idx.PoiYIdx[3].XY = 2.6
+    poi_idx.PoiYIdx[3].ID = 4
+    poi_idx.PoiYIdx[4].XY = 5
+    poi_idx.PoiYIdx[4].ID = 0
+    poi_idx.PoiYIdx[5].XY = 6
+    poi_idx.PoiYIdx[5].ID = 1
+
+    poi_idx.GuidArray[0].GUID = 0
+    poi_idx.GuidArray[0].X = 3
+    poi_idx.GuidArray[0].Y = 5
+    poi_idx.GuidArray[1].GUID = 1
+    poi_idx.GuidArray[1].X = 4
+    poi_idx.GuidArray[1].Y = 6
+    poi_idx.GuidArray[2].GUID = 2
+    poi_idx.GuidArray[2].X = 2.5
+    poi_idx.GuidArray[2].Y = 2.5
+    poi_idx.GuidArray[3].GUID = 3
+    poi_idx.GuidArray[3].X = 4-math.Sqrt(2)
+    poi_idx.GuidArray[3].Y = 4-math.Sqrt(2)
+    poi_idx.GuidArray[4].GUID = 4
+    poi_idx.GuidArray[4].X = 2.6
+    poi_idx.GuidArray[4].Y = 2.6
+    poi_idx.GuidArray[5].GUID = 5
+    poi_idx.GuidArray[5].X = 2
+    poi_idx.GuidArray[5].Y = 2
+
+    var cache distanceTable_t = make(distanceTable_t)
+
+    result := ScanNearPOI(4, 4, poi_idx, 100, 2, cache)
+    assert(len(result) == 4)
+    assert(len(cache) == 6)
+    assert(result[0] == 0)
+    assert(result[1] == 4)
+    assert(result[2] == 3)
+    assert(result[3] == 1)
+
+    poi_idx.PoiXIdx = append(poi_idx.PoiXIdx, Poi_1d_item_t{100.0, 6})
+    poi_idx.PoiYIdx = append(poi_idx.PoiXIdx, Poi_1d_item_t{100.0, 6})
+    poi_idx.GuidArray = append(poi_idx.GuidArray, POI_Item{6, 100.0, 100.0})
+    result = ScanNearPOI(4, 4, poi_idx, 100, 2, cache)
+    assert(len(result) == 4)
+    assert(len(cache) == 6) /* NOTE that cache table is not increased */
+    assert(result[0] == 0)
+    assert(result[1] == 4)
+    assert(result[2] == 3)
+    assert(result[3] == 1)
+}
+
+/* }}} */
+/* {{{ Test_sortby_distance(t *testing.T)  */
+
+func Test_sortby_distance(t *testing.T) {
+    assert := build_assert_func("sortby_distance", t)
+
+    guid_slice := make([]POI_Item, 3)
+    guid_slice[0].GUID = 0
+    guid_slice[0].X = 1
+    guid_slice[0].Y = 3
+    guid_slice[1].GUID = 1
+    guid_slice[1].X = 1
+    guid_slice[1].Y = 2
+    guid_slice[2].GUID = 2
+    guid_slice[2].X = 1
+    guid_slice[2].Y = 1
+
+    ids := []uint32{0, 1, 2}
+    var cache distanceTable_t = make(distanceTable_t)
+    cache[ guid_slice[0] ] = 3
+    cache[ guid_slice[1] ] = 2
+    cache[ guid_slice[2] ] = 1
+
+    sortby_distance(guid_slice, ids, cache)
+    assert(len(ids) == 3)
+    assert(ids[0] == 2)
+    assert(ids[1] == 1)
+    assert(ids[2] == 0)
+
+    ids = []uint32{2, 0, 1}
+    sortby_distance(guid_slice, ids, cache)
+    assert(len(ids) == 3)
+    assert(ids[0] == 2)
+    assert(ids[1] == 1)
+    assert(ids[2] == 0)
+
+    ids = []uint32{1, 2}
+    sortby_distance(guid_slice, ids, cache)
+    assert(len(ids) == 2)
+    assert(ids[0] == 2)
+    assert(ids[1] == 1)
+
+    /* exception test */
+    delete(cache, guid_slice[1])
+    sortby_distance(guid_slice, ids, cache)
+    assert(len(ids) == 2)
+    assert(ids[0] == 2)
+    assert(ids[1] == 0)
+}
+
+/* }}} */
+/* {{{ Test_translate_guid(t *testing.T)  */
+
+func Test_translate_guid(t *testing.T) {
+    assert := build_assert_func("translate_guid() failed", t)
+    guid_slice := make([]POI_Item, 3)
+    guid_slice[0].GUID = 1111
+    guid_slice[0].X = 1
+    guid_slice[0].Y = 3
+    guid_slice[1].GUID = 2222
+    guid_slice[1].X = 1
+    guid_slice[1].Y = 2
+    guid_slice[2].GUID = 3333
+    guid_slice[2].X = 1
+    guid_slice[2].Y = 1
+
+    result := translate_guid(guid_slice, []uint32{0, 2, 1}, 0, 0)
+    assert(len(result) == 3)
+    assert(result[0] == 1111)
+    assert(result[1] == 3333)
+    assert(result[2] == 2222)
+
+    result = translate_guid(guid_slice, []uint32{0, 2, 1}, 1, 1)
+    assert(len(result) == 2)
+    assert(result[0] == 1111)
+    assert(result[1] == 2222)
+
+    result = translate_guid(guid_slice, []uint32{0, 2}, 0, 0)
+    assert(len(result) == 2)
+    assert(result[0] == 1111)
+    assert(result[1] == 3333)
+
+    result = translate_guid(guid_slice, []uint32{0, 2}, 1, 1)
+    assert(len(result) == 1)
+    assert(result[0] == 1111)
+
+    result = translate_guid(guid_slice, []uint32{0, 2}, 1, 2)
+    assert(len(result) == 2)
+    assert(result[0] == 1111)
+    assert(result[1] == 3333)
 }
 
 /* }}} */
